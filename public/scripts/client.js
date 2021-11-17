5cf0e9f385540c233eab74dd3f2f31148cf55612
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-17 01:54:28 +0000

    Set up the counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,30 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
+  $('article').hover(function(){
+    $(this).css("box-shadow", "5px 10px #888888");
+  }, function () {
+    $(this).css("box-shadow", "5px 10px #f4f1ec");
+  });
+  const $colorElement = $('.fa-heart').css('color');
 
+  $('.fa-heart').hover(function(){
+    $(this).css("color", "#ff6666");
+  }, function(){
+    $(this).css("color", $colorElement);
+  })
+
+  $('.fa-flag').hover(function () {
+    $(this).css("color", "#ff6666");
+  }, function () {
+    $(this).css("color", $colorElement);
+  })
+
+  $('.fa-twitter').hover(function () {
+    $(this).css("color", "#ff6666");
+  }, function () {
+    $(this).css("color", $colorElement);
+  })
+
+});

02b1f446a4ac261b2d934a66880ad2aee55915bb
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-19 00:13:57 +0000

    Submit a tweet and update README.md

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 # Tweeter Project
 
-Tweeter is a simple, single-page Twitter clone.
+When the page is loaded we get some data from "http://localhost:8080/tweets"; 
+an API that contains some tweets which we can work with, we get data using 
+$.ajax that fetch tweets asynchronously without refreshing the page.
 
-This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express back-end skills.
+The user can input some text and when they click on the button Tweeter the
+tweet will be added on the top, here also we use $.ajax to stop refreshing the 
+page.
 
-## Getting Started
+The length of the text is by default 140 characters, when the user start typing 
+the length of the zone starts decreasing until it overpasses 140, here if the user
+clicks on submit, they got a message that this tweet is too long
+Also if the zone is empty and the user clicks on submit, they got a message that
+the tweet is empty.
 
-1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
-2. Clone your repository onto your local device.
-3. Install dependencies using the `npm install` command.
-3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
-4. Go to <http://localhost:8080/> in your browser.
-
-## Dependencies
-
-- Express
-- Node 5.10.x or above
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,119 +1,3 @@
-$(document).ready(function() {
-  $('.error').hide();
-  let data = [];
-  $.ajax("http://localhost:8080/tweets", {
-    "user": "user", "content": "content", "created_at": "created_at" })
-    .done(function(dataTweets) {
-      data = dataTweets;
-      console.log(data);
-      renderTweets(data);
-
-  /**
-   * this function makes a shadow when hovering over the article
-   * remove the classe responsible for the border
-   * remove the shadow when outside the article
-   * add the class responsible for the border to the article
-   * NOTE: WE REMOVE THE BORDER AND REPLACE THE BORDER
-   * WE HAVE TO DO THAT BECAUSE THE BORDER WILL BE CUT WHERE THE BORDER
-   * IS MADE
-   */
-      $('article').hover(function() {
-        $(this).css("box-shadow", "5px 10px #888888");
-      }, function() {
-        $(this).removeClass("bord");
-        $(this).css("box-shadow", "5px 10px #f4f1ec");
-        $(this).addClass("bord");
-      });
-      const $colorElement = $('.fa-heart').css('color');
-
-      $('.fa-heart').hover(function() {
-        $(this).css("color", "#ff6666");
-      }, function() {
-        $(this).css("color", $colorElement);
-      });
-
-      $('.fa-flag').hover(function() {
-        $(this).css("color", "#ff6666");
-      }, function() {
-        $(this).css("color", $colorElement);
-      });
-
-      $('.fa-twitter').hover(function() {
-        $(this).css("color", "#ff6666");
-      }, function() {
-        $(this).css("color", $colorElement);
-      });
-
-      $("form").on("submit", function(event) {
-        event.preventDefault();
-        const $text = $(this).serialize();
-        console.log("text ", $text);
-        if($text.trim().length - 5  === 0){
-          $('.empty').show();
-        }
-        else if($text.trim().length > 145 ){
-          $('.long').show();
-        }
-        else{
-          $('.error').hide();
-          $('#tweets-container').empty();
-          data.push({
-            "user":
-            {
-              "name": "Youssef",
-              "avatars": "https://avatars.dicebear.com/api/personas/wwwwwwwwwwwwwur-custom-seed.svg",
-              "handle": "@Youssef"
-            },
-            "content": {
-              "text": $text.replace(/%20/g, " ")
-                .replace(/%C3/g, " ")
-                .replace(/%A8/g, " ")
-                .replace('text=', '')
-                .trim()
-            },
-            "created_at": Date.now()
-          });
-          renderTweets(data.reverse());
-
-        }
-
-        ///////
-        // call ajax 
-        //////
-      });
-    });
-});
-
-const createTweetElement = function (tweetObject) {
-  const $tweet = $(`<article class="tweet bord"></article>`);
-  const $header = $(`<header></header>`);
-  const $avnamehandle = $(`<div class="avnamehandle"></div>`);
-  const $avname = $(`<div class="avname"></div>`);
-  $avname.append(`<img src="${tweetObject.user.avatars}">`);
-  $avname.append(`${tweetObject.user.name}`);
-  $avnamehandle.append($avname);
-  const $handle = $(`<div class="handle"></div>`);
-  $handle.append(`${tweetObject.user.handle}`);
-  $avnamehandle.append($handle);
-  $header.append($avnamehandle);
-  $header.append(`${tweetObject.content.text}`);
-  const $footer = $(`<footer></footer>`);
-  const $dateago = $(`<div class="dateago"></div>`);
-  const $timeago = timeago.format(`${tweetObject.created_at}`);
-  $dateago.append(`${$timeago}`);
-  $footer.append($dateago);
-  $fontawesome = $(`<div class="fontawesome"></div>`);
-  $fontawesome.append(`
-      <i class="fas fa-heart"></i>
-      <i class="fas fa-flag"></i>
-      <i class="fab fa-twitter"></i>
-      `);
-  $footer.append($fontawesome);
-  $tweet.append($header);
-  $tweet.append($footer);
-  return $tweet;
-};
-
 const renderTweets = function (tweets) {
   for (let i = 0; i < tweets.length; i++) {
     const $tweet = createTweetElement(tweets[i]);
@@ -122,6 +6,134 @@ const renderTweets = function (tweets) {
   }
 };
 
+const createTweetElement = function (tweetObject) {
+  const $tweet = $('<article>').addClass('tweet bord');
+  const $header = $('<header>');
+  const $avname = $('<div>')
+    .addClass('avname');
+  const $src = tweetObject.user.avatars;
+  const $image = $('<img>').attr('src', $src);
+  $avname.append($image)
+    .append(tweetObject.user.name)
+  const $handle = $('<div>')
+    .text(tweetObject.user.handle)
+    .addClass('handle');
+
+  const $avnamehandle = $('<div>')
+    .append($avname)
+    .addClass('avnamehandle');
+  $avnamehandle.append($handle);
+  $header.text(tweetObject.content.text);
+  $header.append($avnamehandle);
+
+ 
+  const $footer = $('<footer>');
+  const $timeago = timeago.format(`${tweetObject.created_at}`);
+  const $dateago = $('<div>')
+    .append($timeago)
+    .addClass('dateago');
+
+  $ft_awesome_heart   = $('<i>').addClass('fas fa-heart');
+  $ft_awesome_flag    = $('<i>').addClass('fas fa-flag');
+  $ft_awesome_twitter = $('<i>').addClass('fab fa-twitter');
 
 
+  $fontawesome = $('<div>').addClass('fontawesome');
+  $fontawesome.append($ft_awesome_heart, $ft_awesome_flag, $ft_awesome_twitter);
+  $footer.append($dateago, $fontawesome);
+  $tweet.append($header, $footer);
+  return $tweet;
+};
 
+$(document).ready(function () {
+  const manipulateDOM = function () {
+    $('article').hover(function () {
+      $(this).css("box-shadow", "5px 10px #888888");
+    }, function () {
+      $(this).removeClass("bord");
+      $(this).css("box-shadow", "5px 10px #f4f1ec");
+      $(this).addClass("bord");
+    });
+    const $colorElement = $('.fa-heart').css('color');
+
+    $('.fa-heart').hover(function () {
+      $(this).css("color", "#ff6666");
+    }, function () {
+      $(this).css("color", $colorElement);
+    });
+
+    $('.fa-flag').hover(function () {
+      $(this).css("color", "#ff6666");
+    }, function () {
+      $(this).css("color", $colorElement);
+    });
+
+    $('.fa-twitter').hover(function () {
+      $(this).css("color", "#ff6666");
+    }, function () {
+      $(this).css("color", $colorElement);
+    });
+  }
+
+  $('.error').hide();
+  const BASE_URL = "http://localhost:8080/tweets";
+  let data = [];
+  $.ajax(BASE_URL, {
+    "user": "user", "content": "content", "created_at": "created_at"
+  })
+    .done(function (dataTweets) {
+      data = dataTweets;
+      renderTweets(data);
+
+      /**
+       * this function makes a shadow when hovering over the article
+       * remove the classe responsible for the border
+       * remove the shadow when outside the article
+       * add the class responsible for the border to the article
+       * NOTE: WE REMOVE THE BORDER AND REPLACE THE BORDER
+       * WE HAVE TO DO THAT BECAUSE THE BORDER WILL BE CUT WHERE THE BORDER
+       * IS MADE
+       */
+      manipulateDOM();
+      $("form").on("submit", function (event) {
+        event.preventDefault();
+        let $text = $(this).serialize();
+        $text = $text.substring(5);
+        $text = decodeURI($text);
+        $text = $text.replace(/%20/g, " ")
+          .replace(/%C3/g, " ")
+          .replace(/%A8/g, " ")
+          .replace(/%3A/g, " ")
+          .trim();
+        if ($text.trim().length === 0) {
+          $('.empty').show();
+        }
+        else if ($text.trim().length >= 140) {
+          $('.long').show();
+        }
+        else {
+          if($text.length >= 80){
+            let $text1 = $text.substring(0, 80) + '\n';
+            $text = $text1 + $text.substring(81, $text.length);
+          }
+          $('.error').hide();
+          $('#tweets-container').empty();
+          data.push({
+            "user":
+            {
+              "name": "Youssef",
+              "avatars": "https://avatars.dicebear.com/api/personas/wwwwwwwwwwwwwur-custom-seed.svg",
+              "handle": "@nafaay"
+            },
+            "content": {
+              "text": $text
+            },
+            "created_at": Date.now()
+          });
+          renderTweets(data.reverse());
+          manipulateDOM();
+
+        }
+      });
+    });
+});

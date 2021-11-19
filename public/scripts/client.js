1d2096d82d894653f9d264ec0044ce5bc4e214b5
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-19 00:39:23 +0000

    Clean the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,5 @@
-const renderTweets = function (tweets) {
+// render all tweets from the array of tweets
+const renderTweets = function(tweets) {
   for (let i = 0; i < tweets.length; i++) {
     const $tweet = createTweetElement(tweets[i]);
     $('#tweets-container').append($tweet);
@@ -6,7 +7,13 @@ const renderTweets = function (tweets) {
   }
 };
 
-const createTweetElement = function (tweetObject) {
+/**
+ *function that creates a DOM article as a variable
+  $tweet and its descendants, and return that variable
+  this function will be called once we load the data
+  from the internet, and at each time we submit a tweet.
+ */
+const createTweetElement = function(tweetObject) {
   const $tweet = $('<article>').addClass('tweet bord');
   const $header = $('<header>');
   const $avname = $('<div>')
@@ -14,7 +21,7 @@ const createTweetElement = function (tweetObject) {
   const $src = tweetObject.user.avatars;
   const $image = $('<img>').attr('src', $src);
   $avname.append($image)
-    .append(tweetObject.user.name)
+    .append(tweetObject.user.name);
   const $handle = $('<div>')
     .text(tweetObject.user.handle)
     .addClass('handle');
@@ -33,69 +40,70 @@ const createTweetElement = function (tweetObject) {
     .append($timeago)
     .addClass('dateago');
 
-  $ft_awesome_heart   = $('<i>').addClass('fas fa-heart');
-  $ft_awesome_flag    = $('<i>').addClass('fas fa-flag');
-  $ft_awesome_twitter = $('<i>').addClass('fab fa-twitter');
+  const $ftAwesomeHeart   = $('<i>').addClass('fas fa-heart');
+  const $ftAwesomeFlag    = $('<i>').addClass('fas fa-flag');
+  const $ftAwesomeTwitterr = $('<i>').addClass('fab fa-twitter');
 
 
-  $fontawesome = $('<div>').addClass('fontawesome');
-  $fontawesome.append($ft_awesome_heart, $ft_awesome_flag, $ft_awesome_twitter);
+  const $fontawesome = $('<div>').addClass('fontawesome');
+  $fontawesome.append($ftAwesomeHeart, $ftAwesomeFlag, $ftAwesomeTwitterr);
   $footer.append($dateago, $fontawesome);
   $tweet.append($header, $footer);
   return $tweet;
 };
 
-$(document).ready(function () {
-  const manipulateDOM = function () {
-    $('article').hover(function () {
+/**
+ * Here we can use our functions after the document is ready
+ */
+$(document).ready(function() {
+  /**
+   * function that will use some charcteristics of jQuery to manipulate
+   * the DOM, like hover, addClass, removeClass...etc
+   */
+  const manipulateDOM = function() {
+    $('article').hover(function() {
       $(this).css("box-shadow", "5px 10px #888888");
-    }, function () {
+    }, function() {
       $(this).removeClass("bord");
       $(this).css("box-shadow", "5px 10px #f4f1ec");
       $(this).addClass("bord");
     });
     const $colorElement = $('.fa-heart').css('color');
 
-    $('.fa-heart').hover(function () {
+    $('.fa-heart').hover(function() {
       $(this).css("color", "#ff6666");
-    }, function () {
+    }, function() {
       $(this).css("color", $colorElement);
     });
 
-    $('.fa-flag').hover(function () {
+    $('.fa-flag').hover(function() {
       $(this).css("color", "#ff6666");
-    }, function () {
+    }, function() {
       $(this).css("color", $colorElement);
     });
 
-    $('.fa-twitter').hover(function () {
+    $('.fa-twitter').hover(function() {
       $(this).css("color", "#ff6666");
-    }, function () {
+    }, function() {
       $(this).css("color", $colorElement);
     });
-  }
+  };
 
+  /**
+   * $.ajax is asynchronous, we have to encapsulate all the code after
+   * inside the call back to avoid having undefined variables
+   */
   $('.error').hide();
   const BASE_URL = "http://localhost:8080/tweets";
   let data = [];
   $.ajax(BASE_URL, {
     "user": "user", "content": "content", "created_at": "created_at"
   })
-    .done(function (dataTweets) {
+    .done(function(dataTweets) {
       data = dataTweets;
       renderTweets(data);
-
-      /**
-       * this function makes a shadow when hovering over the article
-       * remove the classe responsible for the border
-       * remove the shadow when outside the article
-       * add the class responsible for the border to the article
-       * NOTE: WE REMOVE THE BORDER AND REPLACE THE BORDER
-       * WE HAVE TO DO THAT BECAUSE THE BORDER WILL BE CUT WHERE THE BORDER
-       * IS MADE
-       */
       manipulateDOM();
-      $("form").on("submit", function (event) {
+      $("form").on("submit", function(event) {
         event.preventDefault();
         let $text = $(this).serialize();
         $text = $text.substring(5);
@@ -107,12 +115,10 @@ $(document).ready(function () {
           .trim();
         if ($text.trim().length === 0) {
           $('.empty').show();
-        }
-        else if ($text.trim().length >= 140) {
+        } else if ($text.trim().length >= 140) {
           $('.long').show();
-        }
-        else {
-          if($text.length >= 80){
+        } else {
+          if ($text.length >= 80) {
             let $text1 = $text.substring(0, 80) + '\n';
             $text = $text1 + $text.substring(81, $text.length);
           }

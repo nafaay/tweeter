c010963cd10bd7775d28604793b81280b14efbbe
Youssef Nafaa <nafaayoussef@gmail.com>
2021-12-02 07:05:13 -0500

    get all data after refresh page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,96 +1,90 @@
-// render all tweets from the array of tweets
-const renderTweets = function(tweets) {
-  for (let i = 0; i < tweets.length; i++) {
-    const $tweet = createTweetElement(tweets[i]);
-    $('#tweets-container').append($tweet);
-    $('#tweets-container').append('<br>');
-  }
-};
-
-/**
- *function that creates a DOM article as a variable
-  $tweet and its descendants, and return that variable
-  this function will be called once we load the data
-  from the internet, and at each time we submit a tweet.
- */
-const createTweetElement = function(tweetObject) {
-  const $tweet = $('<article>').addClass('tweet bord');
-  const $header = $('<header>');
-  const $avname = $('<div>')
-    .addClass('avname');
-  const $src = tweetObject.user.avatars;
-  const $image = $('<img>').attr('src', $src);
-  $avname.append($image)
-    .append(tweetObject.user.name);
-  const $handle = $('<div>')
-    .text(tweetObject.user.handle)
-    .addClass('handle');
-
-  const $avnamehandle = $('<div>')
-    .append($avname)
-    .addClass('avnamehandle');
-  $avnamehandle.append($handle);
-  $header.text(tweetObject.content.text);
-  $header.append($avnamehandle);
-
- 
-  const $footer = $('<footer>');
-  const $timeago = timeago.format(`${tweetObject.created_at}`);
-  const $dateago = $('<div>')
-    .append($timeago)
-    .addClass('dateago');
-
-  const $ftAwesomeHeart   = $('<i>').addClass('fas fa-heart');
-  const $ftAwesomeFlag    = $('<i>').addClass('fas fa-flag');
-  const $ftAwesomeTwitterr = $('<i>').addClass('fab fa-twitter');
-
-
-  const $fontawesome = $('<div>').addClass('fontawesome');
-  $fontawesome.append($ftAwesomeHeart, $ftAwesomeFlag, $ftAwesomeTwitterr);
-  $footer.append($dateago, $fontawesome);
-  $tweet.append($header, $footer);
-  return $tweet;
-};
-
 /**
  * Here we can use our functions after the document is ready
  */
-$(document).ready(function() {
+$(document).ready(() => {
+  const renderTweets = function (tweets) {
+    $('#tweets-container').empty();
+    for (let i = 0; i < tweets.length; i++) {
+      const $tweet = createTweetElement(tweets[i]);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
+  const getTweets = function () {
+    $.get('/tweets', (data) => {
+      renderTweets(data);
+    });
+  };
+
+  getTweets();
   /**
    * function that will use some charcteristics of jQuery to manipulate
    * the DOM, like hover, addClass, removeClass...etc
    */
-  const manipulateDOM = function() {
-    $('article').hover(function() {
-      $(this).css("box-shadow", "5px 10px #888888");
-    }, function() {
-      $(this).removeClass("bord");
-      $(this).css("box-shadow", "5px 10px #f4f1ec");
-      $(this).addClass("bord");
-    });
-    // Clear textarea after submitting tweet.
-    $('#tweet-text').val("");
-    $('.counter').text('140'); 
-    
+
+  const manipulateDOM = function () {
     const $colorElement = $('.fa-heart').css('color');
-    $('.fa-heart').hover(function() {
-      $(this).css("color", "#ff6666");
-    }, function() {
-      $(this).css("color", $colorElement);
+    $('.fa-heart').hover(function () {
+      $(this).css('color', '#ff6666');
+    }, function () {
+      $(this).css('color', $colorElement);
     });
 
-    $('.fa-flag').hover(function() {
-      $(this).css("color", "#ff6666");
-    }, function() {
-      $(this).css("color", $colorElement);
+    $('.fa-flag').hover(function () {
+      $(this).css('color', '#ff6666');
+    }, function () {
+      $(this).css('color', $colorElement);
     });
 
-    $('.fa-twitter').hover(function() {
-      $(this).css("color", "#ff6666");
-    }, function() {
-      $(this).css("color", $colorElement);
+    $('.fa-twitter').hover(function () {
+      $(this).css('color', '#ff6666');
+    }, function () {
+      $(this).css('color', $colorElement);
     });
 
+    $('article').hover(function () {
+      $(this).css('box-shadow', '5px 10px #888888');
+    }, function () {
+      $(this).removeClass('bord');
+      $(this).css('box-shadow', '5px 10px #f4f1ec');
+      $(this).addClass('bord');
+    });
+  };
+
+  const createTweetElement = function (data) {
+    const $tweet = $('<article>').addClass('tweet bord');
+    const $header = $('<header>');
+    const $avname = $('<div>')
+      .addClass('avname');
+    const $src = data.user.avatars;
+    const $image = $('<img>').attr('src', $src);
+    $avname.append($image)
+      .append(data.user.name);
+    const $handle = $('<div>')
+      .text(data.user.handle)
+      .addClass('handle');
+
+    const $avnamehandle = $('<div>')
+      .append($avname)
+      .addClass('avnamehandle');
+    $avnamehandle.append($handle);
+    $header.text(data.content.text);
+    $header.append($avnamehandle);
+
+    const $footer = $('<footer>');
+    const $timeago = timeago.format(`${data.created_at}`);
+    const $dateago = $('<div>')
+      .append($timeago)
+      .addClass('dateago');
+
+    const $ftAwesomeHeart = $('<i>').addClass('fas fa-heart');
+    const $ftAwesomeFlag = $('<i>').addClass('fas fa-flag');
+    const $ftAwesomeTwitterr = $('<i>').addClass('fab fa-twitter');
+
+    const $fontawesome = $('<div>').addClass('fontawesome');
+    $fontawesome.append($ftAwesomeHeart, $ftAwesomeFlag, $ftAwesomeTwitterr);
+    $footer.append($dateago, $fontawesome);
+    $tweet.append($header, $footer);
+    return $tweet;
   };
 
   /**
@@ -98,50 +92,32 @@ $(document).ready(function() {
    * inside the call back to avoid having undefined variables
    */
   $('.error').hide();
-  const BASE_URL = "http://localhost:8080/tweets";
+  const BASE_URL = 'http://localhost:8080/tweets';
   $.ajax(BASE_URL, {
-    "user": "user", "content": "content", "created_at": "created_at"
+    user: 'user', content: 'content', created_at: 'created_at',
   })
-  .done(function(dataTweets) {
+    .done((dataTweets) => {
       data = dataTweets;
       renderTweets(data);
       manipulateDOM();
-      $("form").on("submit", function(event) {
+      $('form').submit(function (event) {
         event.preventDefault();
-        let $text = $(this).serialize();
-        $text = $text.substring(5);
-        $text = decodeURI($text);
-        $text = $text.replace(/%20/g, " ")
-          .replace(/%C3/g, " ")
-          .replace(/%A8/g, " ")
-          .replace(/%3A/g, " ")
-          .trim();
-        if ($text.trim().length === 0) {
-          $('.empty').show();
-        } else if ($text.trim().length >= 140) {
+        const data = $(this).serialize();
+        const tweetLength = $('#tweet-text').val().length;
+
+        if (tweetLength > 140) {
           $('.long').show();
+        } else if (tweetLength === 0) {
+          $('.empty').show();
         } else {
-          if ($text.length >= 80) {
-            let $text1 = $text.substring(0, 80) + '\n';
-            $text = $text1 + $text.substring(81, $text.length);
-          }
           $('.error').hide();
-          $('#tweets-container').empty();
-          data.push({
-            "user":
-            {
-              "name": "Youssef",
-              "avatars": "https://avatars.dicebear.com/api/personas/wwwwwwwwwwwwwur-custom-seed.svg",
-              "handle": "@nafaay"
-            },
-            "content": {
-              "text": $text
-            },
-            "created_at": Date.now()
+          $.post('/tweets', data, () => {
+            getTweets();
+            $('#tweet-text').val('');
+            $('.counter').text('140');
+            renderTweets(data);
+            manipulateDOM();
           });
-          renderTweets(data.reverse());
-          manipulateDOM();
-          document.cookie = data;
         }
       });
     });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,17 @@
 $(document).ready(function () {
-  $('#tweet-text').focus(function() {
+  $('#tweet-text').focus(function () {
     $(this).css("background-color", "#e0ebeb");
   });
 
   const $colorCounter = $('.counter').css('color');
   const $counterVal = $('.counter').val();
   $('#tweet-text').keyup(function () {
-      $('.counter').val($counterVal - $(this).val().length);
-      if ($('.counter').val() <= 0 ){
-        $('.counter').css('color', 'red');
-      }
-      else{
-        $('.counter').css('color', $colorCounter);
-      }
+    $('.counter').val($counterVal - $(this).val().length);
+    if ($('.counter').val() <= 0) {
+      $('.counter').css('color', 'red');
+    }
+    else {
+      $('.counter').css('color', $colorCounter);
+    }
   });
 });
-
